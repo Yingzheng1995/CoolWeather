8eafaf1c343cad8fa33ac6df06f04cb2311a305f
yz x <yz x>
2017-02-16 18:54:36 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/hitwh/xyz/coolweatherbyxyz/util/HttpUtil.java
+++ app/src/main/java/hitwh/xyz/coolweatherbyxyz/util/HttpUtil.java
@@ -6,12 +6,22 @@ import okhttp3.Request;
 /**
  * Created by ASUS on 2017/2/15.
  */
-
+/**
+ *
+ * @author 许颖正
+ *
+ */
 public class HttpUtil {
+    /**
+     *
+     * @param address  目标链接地址
+     * @param callback 接口内有onResponse 和 onFailure 两个方法
+     */
     public static void sendOkHttpRequest(String address,okhttp3.Callback callback)
     {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
+
     }
 }
---
+++ app/src/main/java/hitwh/xyz/coolweatherbyxyz/util/Utility.java
@@ -0,0 +1,104 @@
+package hitwh.xyz.coolweatherbyxyz.util;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import hitwh.xyz.coolweatherbyxyz.db.City;
+import hitwh.xyz.coolweatherbyxyz.db.County;
+import hitwh.xyz.coolweatherbyxyz.db.Province;
+
+/**
+ * Created by ASUS on 2017/2/15.
+ */
+
+
+public class Utility {
+    /**
+     * 解析和处理服务器返回的省级数据
+     * @param response
+     * @return 成功或失败
+     */
+    private static final String TAG = "Utility";
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i=0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setPeovinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * 解析和处理服务器返回的市级数据
+     * @param response 响应信息
+     * @param provinceId 所在省的编号
+     * @return 成功或失败
+     */
+    public static boolean handleCityResponse(String response,int provinceId){
+        if (!TextUtils.isEmpty(response)) {
+            try {
+//                Log.d(TAG, "handleCityResponse: "+response);
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0;i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceCode(provinceId);
+                    city.setCityName(cityObject.getString("name"));
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        return false;
+    }
+
+    /**
+     *  解析和处理服务器返回的县级数据
+     * @param response 获得的响应
+     * @param cityId 所在市的编号
+     * @return 成功或失败
+     */
+
+    public static boolean handleCountyResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i=0;i<allCounties.length();i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCityId(cityId);
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.save();
+
+                }
+                return true;
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
